b1b8cdd923c8430acc36b4739803e4430c4ee222
root-ashutosh <ashutoshgamharia@gmail.com>
2025-06-22 18:20:34 +0530

    usermodel changed

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -2,14 +2,13 @@ import mongoose from "mongoose";
 
 
 const userSchema = new mongoose.Schema({
-    clerkId: { type: String, required: true, unique: true},
+    clerkId: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     photo: { type: String, required: true },
     firstName: { type: String },
     lastName: { type: String },
-    creditBalance: { type: Number,default:5 } 
-
-})
+    creditBalance: { type: Number, default: 5 }
+});
 
 // if the model is already created then use it else create a new model 
 const userModel = mongoose.models.user || mongoose.model('user',userSchema);
