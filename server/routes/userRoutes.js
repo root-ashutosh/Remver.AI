95540062ebedd47378d64492b6245767e5c3ee0c
root-ashutosh <ashutoshgamharia@gmail.com>
2025-06-22 17:53:53 +0530

    db

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,3 @@
-// import express from 'express'
-// import { clerkWebhooks } from '../controllers/userController.js'
-
-// const userRouter = express.Router()
-
-// userRouter.post('/webhooks',clerkWebhooks)
-
-// export default userRouter
-
 import express from 'express';
 import { clerkWebhooks } from '../controllers/userController.js';
 
--- server/server.js
+++ server/server.js
@@ -1,26 +1,3 @@
-// import 'dotenv/config'
-// import express from 'express'
-// import cors from 'cors'
-// import connectDB from './config/mongodb.js'
-// import userRouter from './routes/userRoutes.js'
-
-// // APP config
-// const PORT = process.env.PORT || 4000
-// const app = express() //initialising instance of express
-// await connectDB()
-
-// // API routes
-// app.get('/',(req,res)=> res.send("Api working"))
-
-// app.use('/api/user',userRouter)
-
-
-// // initialize middleware
-// app.use(express.json()) 
-// app.use(cors()) 
-
-// app.listen(PORT, ()=> console.log('Server running on PORT:' +PORT))
-
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
